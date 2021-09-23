d343d26a1664de32deb6e56e4d06b1e6a75d3d3d
Gustavo Barboza <gustavobarboza.oliveira@gmail.com>
2021-09-22 21:42:11 -0300

    updated menu items to navigate with router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component'
+
+const HatsPage = (props)=> {
+  console.log(props) 
+  return (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+  )
+  }
+
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path="/" component={HomePage} />
+      <Route path="/shop/hats" component={HatsPage} />
     </div>
   );
 }
